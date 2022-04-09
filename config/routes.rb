cd6aa53aca0c61f7a1b754679d6918bff92c7b5b
eri-Q <eri.hatanaka8@gmail.com>
2022-04-09 14:18:42 +0000

    book投稿機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'about', to: 'homes#about'
+  resources :users, only: [:index, :show, :edit, :update]
+  resources :books
 end
