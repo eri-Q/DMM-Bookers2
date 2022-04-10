9f1dd003b1da957544be13832a855daebb3e5006
eri-Q <eri.hatanaka8@gmail.com>
2022-04-10 15:18:51 +0000

    UserHome

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -17,15 +17,23 @@ class BooksController < ApplicationController
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to root_path
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to root_path
   end
 
   private
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,15 +1,30 @@
 class UsersController < ApplicationController
-  
+
   def index
+    @users = User.all
+    @user = User.find(current_user.id)
+    @book = Book.new
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = Book.all
   end
-  
+
   def edit
   end
 
   def update
   end
 
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :introduction)
+  end
 end
